8cdc701c94960f37c1f8d206ab0ecf82887235d3
Tatsuo Indou <tatsuo-i@mtb.biglobe.ne.jp>
2013-02-14 00:01:42 +0900

    add get-folder-size.cmd if sattements

--- get-folder-size.cmd
+++ get-folder-size.cmd
@@ -1,13 +1,26 @@
 @echo off
 set CWD=%~dp0
 set JS=%CWD%\%~n0.wsf
-set RESULT=%CWD%\%~n0.bs
-set AFTERSORT=%CWD%\%~n0.as
+set RESULT=%~n0.bs
+set AFTERSORT=%~n0.as
 
 echo start
 echo %1
 cscript //nologo %JS% %1 > %RESULT%
 set rc=%errorlevel%
-sort /r %RESULT% > %AFTERSORT%
 echo %rc%
+if %rc% neq 0 goto :abnormal
+
+sort /r %RESULT% > %AFTERSORT%
+set rc=%errorlevel%
+echo %rc%
+if %rc% neq 0 goto :abnormal
+
+echo %AFTERSORT%‚ğŠm”F‚µ‚Ä‚­‚¾‚³‚¢B
 echo done
+
+:success
+exit /b 0
+
+:abnormal
+exit /b 1
